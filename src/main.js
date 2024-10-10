edfdbd4473825e35290b2ace3898793bed627e69
LeonardoKako <fleithkako@gmail.com>
2024-10-10 11:44:17 -0300

    ajustes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll(['data-tab-button']);
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question');
+    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+
+        if(posicaoAtual < alturaHero) {
+            ocultaElementoHeader();
+        } else {
+            exibeElementoHeader();
+        }
+    })
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -12,7 +25,28 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+        
+    }
 })
+
+function ocultaElementoHeader() {
+    const header = document.querySelector('.header')
+    header.classList.add('header--is-hidden')
+}
+function exibeElementoHeader() {
+    const header = document.querySelector('.header')
+    header.classList.remove('header--is-hidden')
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    console.log(elemento);
+    const elementoPai = elemento.target.parentNode;
+    elementoPai.classList.toggle(classe)
+}
 
 
 function removeBotaoAtivo() {
